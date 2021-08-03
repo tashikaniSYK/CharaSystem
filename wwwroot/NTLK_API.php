5dcfe2acad2016fb1acaa6ec774426968ee671b4
StevenGerry <st_hwh@yahoo.co.jp>
2021-08-03 17:10:50 +0900

    update php allow CORS

--- wwwroot/NTLK_API.php
+++ wwwroot/NTLK_API.php
@@ -3,6 +3,11 @@ error_reporting(E_ALL^E_NOTICE^E_WARNING);
 mb_language("Japanese");
 mb_internal_encoding("UTF-8");
 mb_regex_encoding('UTF-8');
+header("access-control-allow-headers: Accept,Authorization,Cache-Control,Content-Type,DNT,If-Modified-Since,Keep-Alive,Origin,User-Agent,X-Mx-ReqToken,X-Requested-With");
+header("access-control-allow-methods: GET, POST, PUT, DELETE, HEAD, OPTIONS");
+header("access-control-allow-credentials: true");
+header("access-control-allow-origin: *");
+header('X-Powered-By: WAF/2.0');
 function unicode_decode($name)  
 {  
     $name = str_replace("\\\\u","\u",$name);
@@ -44,7 +49,7 @@ class CallBackData
 $data = new CallBackData();
 $postget = $_POST["text"];
 $receive = $_POST["token"];
-if ($receive=='')
+if ($receive=='0mEaz!0j2kAGXXa9')
 {
 	#$epostget = unicode_decode($postget);
 	$cmd = 'python3 NTLK_API.py '.$postget.' 2>&1';
